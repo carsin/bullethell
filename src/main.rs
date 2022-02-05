069adb7d6f69cf5273b4dc288432917a38eb596b
carsin <20868514+carsin@users.noreply.github.com>
2022-02-05 00:52:32 -0800

    bullets

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}, transform};
 
 const SPEED: f32 = 100.;
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Bullet {
+    speed: i32,
+}
+
+#[derive(Clone)]
+struct BulletAssets {
+    mesh: Mesh2dHandle,
+    material: Handle<ColorMaterial>,
+}
 
 fn update_player(keys: Res<Input<KeyCode>>, time: Res<Time>, mut query: Query<(&Player, &mut Transform)>) {
     for (player, mut transform) in query.iter_mut() {
@@ -36,6 +47,34 @@ fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut ma
         });
 }
 
+fn load_bullet_mesh(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.insert_resource(BulletAssets {
+        mesh: meshes.add(Mesh::from(shape::Quad {
+            size: Vec2::new(2., 10.),
+            flip: false,
+        })).into(),
+        material: materials.add(ColorMaterial::from(Color::rgb(0.7, 0.7, 0.1))),
+    });
+}
+
+fn spawn_bullet(mut commands: Commands, assets: Res<BulletAssets>) {
+    commands.spawn()
+        .insert(Bullet {
+            speed: 10,
+        })
+        .insert_bundle(MaterialMesh2dBundle {
+            mesh: assets.mesh.clone(),
+            material: assets.material.clone(),
+            ..Default::default()
+        });
+}
+
+fn update_bullets(mut query: Query<(&Bullet, &mut Transform)>) {
+    for (bullet, mut transform) in query.iter_mut() {
+        transform.translation.y += bullet.speed as f32;
+    }
+}
+
 fn spawn_camera(mut commands: Commands) {
     commands.spawn().insert_bundle(OrthographicCameraBundle::new_2d());
 }
@@ -47,8 +86,11 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(load_bullet_mesh)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_bullet)
         .add_system(update_player)
+        .add_system(update_bullets)
         .run();
 }

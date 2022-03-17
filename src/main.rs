a6a0018eb372f725c17b6549bd40909c4daf9b7d
carsin <20868514+carsin@users.noreply.github.com>
2022-03-17 12:26:55 -0700

    fix bullet fire direction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use bevy::{prelude::*, sprite::{MaterialMesh2dBundle, Mesh2dHandle}, math::vec2};
+use bevy::{
+    math::vec2,
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
 const PLAYER_SPEED: f32 = 500.;
 const BULLET_SPEED: f32 = 1000.;
@@ -12,6 +16,7 @@ struct Player {
 struct Bullet {
     speed: f32,
     dir: Vec2,
+    // rot: Vec2,
 }
 
 #[derive(Clone)]
@@ -26,26 +31,26 @@ struct BulletFireEvent {
 }
 
 fn update_player(
-    keys: Res<Input<KeyCode>>, 
-    mouse: Res<Input<MouseButton>>, 
-    time: Res<Time>, 
+    keys: Res<Input<KeyCode>>,
+    mouse: Res<Input<MouseButton>>,
+    time: Res<Time>,
     windows: Res<Windows>, // does this need to be retrieved every update?
     mut write_bullet: EventWriter<BulletFireEvent>,
-    mut query: Query<(&Player, &mut Transform)>
+    mut query: Query<(&Player, &mut Transform)>,
 ) {
     for (player, mut transform) in query.iter_mut() {
         if keys.pressed(KeyCode::W) {
             transform.translation.y += player.speed * time.delta_seconds();
         }
-        
+
         if keys.pressed(KeyCode::A) {
             transform.translation.x -= player.speed * time.delta_seconds();
         }
-        
+
         if keys.pressed(KeyCode::S) {
             transform.translation.y -= player.speed * time.delta_seconds();
         }
-        
+
         if keys.pressed(KeyCode::D) {
             transform.translation.x += player.speed * time.delta_seconds();
         }
@@ -55,11 +60,14 @@ fn update_player(
             if let Some(click_pos) = window.cursor_position() { // cursor is within window
                 // get difference vector
                 let player_pos = vec2(transform.translation.x, transform.translation.y);
-                let dir_vec = (click_pos - player_pos).normalize();
-                
+                //  click pos origin (0,0) at bottom-left
+                let dir_vec = (vec2(click_pos.x - (window.width() / 2.), click_pos.y - (window.height() / 2.)) - player_pos).normalize();
+                println!("fire event:\n player_pos: {}\n click_pos: {}\n", player_pos, click_pos);
+
                 // fire bullet
-                write_bullet.send(BulletFireEvent {  // send event
-                    pos: Vec2::new(transform.translation.x, transform.translation.y) ,
+                write_bullet.send(BulletFireEvent {
+                    // send event
+                    pos: Vec2::new(transform.translation.x, transform.translation.y),
                     dir: dir_vec,
                 });
             }
@@ -67,32 +75,49 @@ fn update_player(
     }
 }
 
-fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
-    commands.spawn()
+fn spawn_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands
+        .spawn()
         .insert(Player {
             speed: PLAYER_SPEED,
         })
-        .insert_bundle(MaterialMesh2dBundle { 
+        .insert_bundle(MaterialMesh2dBundle {
             mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-            transform: Transform::default().with_scale(Vec3::splat(50.)), material: materials.add(ColorMaterial::from(Color::rgb(0.1, 1., 0.1))),
+            transform: Transform::default().with_scale(Vec3::splat(50.)),
+            material: materials.add(ColorMaterial::from(Color::rgb(0.1, 1., 0.1))),
             ..Default::default()
         });
 }
 
-fn load_bullet_mesh(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+fn load_bullet_mesh(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.insert_resource(BulletAssets {
-        mesh: meshes.add(Mesh::from(shape::Quad {
-            size: Vec2::new(2., 10.),
-            flip: false,
-        })).into(),
+        mesh: meshes
+            .add(Mesh::from(shape::Quad {
+                size: Vec2::new(2., 10.),
+                flip: false,
+            }))
+            .into(),
         material: materials.add(ColorMaterial::from(Color::rgb(1.0, 1.0, 0.1))),
     });
 }
 
 // spawns bullet on bulletfireevent send
-fn spawn_bullet(mut commands: Commands, assets: Res<BulletAssets>, mut listen_bullet: EventReader<BulletFireEvent>) {
+fn spawn_bullet(
+    mut commands: Commands,
+    assets: Res<BulletAssets>,
+    mut listen_bullet: EventReader<BulletFireEvent>,
+) {
     for fire in listen_bullet.iter() {
-        commands.spawn()
+        commands
+            .spawn()
             .insert(Bullet {
                 speed: BULLET_SPEED,
                 dir: fire.dir,
@@ -100,28 +125,32 @@ fn spawn_bullet(mut commands: Commands, assets: Res<BulletAssets>, mut listen_bu
             .insert_bundle(MaterialMesh2dBundle {
                 mesh: assets.mesh.clone(),
                 material: assets.material.clone(),
-                transform: Transform::default().with_translation(Vec3::new(fire.pos.x, fire.pos.y, 0.0)),
+                transform: Transform::default()
+                    .with_translation(Vec3::new(fire.pos.x, fire.pos.y, 0.0)),
                 ..Default::default()
             });
     }
 }
 
-fn update_bullets(mut query: Query<(&Bullet, &mut Transform)>, time: Res<Time>,) {
+fn update_bullets(mut query: Query<(&Bullet, &mut Transform)>, time: Res<Time>) {
     for (bullet, mut transform) in query.iter_mut() {
-        // transform.rotate()
+        // rotate bullet by angle from x-axis
+        // transform.rotate(Quat::from_rotation_x(bullet.rot));
         transform.translation.x += (bullet.dir.x * bullet.speed) * time.delta_seconds();
         transform.translation.y += (bullet.dir.y * bullet.speed) * time.delta_seconds();
     }
 }
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn().insert_bundle(OrthographicCameraBundle::new_2d());
+    commands
+        .spawn()
+        .insert_bundle(OrthographicCameraBundle::new_2d());
 }
 
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
-            title: "Game !dwmfloat".to_string(),
+            title: "bullethell prototype 'corpseCo' !dwmf".to_string(),
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)

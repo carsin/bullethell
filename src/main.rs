151114fd0030de523be26387148080908aacf286
carsin <20868514+carsin@users.noreply.github.com>
2023-01-05 20:00:35 -0800

    player movement

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 
 enum QueueEvent {
-    TravelEvent,
+    TravelEvent(Vec2),
+    AttackEvent,
 }
 
 #[derive(Component)]
-struct Entity {
-    command_queue: Vec<QueueEvent>,
-}
+struct CommandQueue(Vec<QueueEvent>);
+
+#[derive(Component)]
+struct Entity;
 
 #[derive(Component)]
 struct Kind(String);
@@ -16,12 +18,14 @@ struct Kind(String);
 #[derive(Component)]
 struct Player(f32);
 
+fn graphics_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn player_setup(mut commands: Commands, mut rapier_conf: ResMut<RapierConfiguration>) {
     rapier_conf.gravity = Vec2::ZERO;
-    commands.spawn(Camera2dBundle::default());
-    
-    let sprite_size = 50.;
 
+    let sprite_size = 50.;
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
@@ -33,29 +37,39 @@ fn player_setup(mut commands: Commands, mut rapier_conf: ResMut<RapierConfigurat
         },
         RigidBody::Dynamic,
         Velocity::zero(),
-        Collider::ball(sprite_size / 2.),
+        Collider::cuboid(sprite_size / 2., sprite_size / 2.),
         Player(50.0),
     ));
 }
 
-fn player_movement(input: Res<Input<KeyCode>>, mut player_query: Query<(&Player, &mut Velocity)>) {
+fn player_movement(
+    time: Res<Time>,
+    keys: Res<Input<KeyCode>>,
+    mut player_query: Query<(&Player, &mut Velocity)>,
+) {
     for (player, mut rigid_body) in &mut player_query {
-        let up = input.pressed(KeyCode::W);
-        let down = input.pressed(KeyCode::S);
-        let left = input.pressed(KeyCode::A);
-        let right = input.pressed(KeyCode::D);
-
-        let x_axis = -(left as i8) + right as i8;
-        let y_axis = -(down as i8) + up as i8;
-
-        let mut delta = Vec2::new(x_axis as f32, y_axis as f32);
-        if delta != Vec2::ZERO {
-            delta /= delta.length();
+        let mut dir = Vec2::ZERO;
+        if keys.pressed(KeyCode::A) {
+            dir -= Vec2::new(1.0, 0.0);
         }
+
+        if keys.pressed(KeyCode::D) {
+            dir += Vec2::new(1.0, 0.0);
+        }
+
+        if keys.pressed(KeyCode::W) {
+            dir += Vec2::new(0.0, 1.0);
+        }
+
+        if keys.pressed(KeyCode::S) {
+            dir -= Vec2::new(0.0, 1.0);
+        }
+
+        let speed = 200.;
+        let delta = dir * time.delta_seconds() * speed;
         // update linear velocity
         rigid_body.linvel = delta * player.0;
     }
-    
 }
 
 fn main() {
@@ -71,6 +85,9 @@ fn main() {
             ..default()
         }))
         .add_startup_system(player_setup)
+        .add_startup_system(graphics_setup)
         .add_system(player_movement)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.))
+        .add_plugin(RapierDebugRenderPlugin::default())
         .run();
 }

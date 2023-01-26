8c9d5661dea449421f870f49bf6b68d41ac7689e
carsin <20868514+carsin@users.noreply.github.com>
2023-01-26 11:13:15 -1000

    progress

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn graphics_setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
+// setup player
 fn player_setup(mut commands: Commands, mut rapier_conf: ResMut<RapierConfiguration>) {
     rapier_conf.gravity = Vec2::ZERO;
 
@@ -35,14 +36,84 @@ fn player_setup(mut commands: Commands, mut rapier_conf: ResMut<RapierConfigurat
             },
             ..Default::default()
         },
+        Player(50.0),
+        CommandQueue(Vec::new()),
+        // rapier
         RigidBody::Dynamic,
         Velocity::zero(),
         Collider::cuboid(sprite_size / 2., sprite_size / 2.),
-        Player(50.0),
     ));
 }
 
-fn player_movement(
+// setup another entity
+// https://rapier.rs/docs/user_guides/bevy_plugin/rigid_bodies
+fn entity_setup(mut commands: Commands) {
+    let sprite_size = 20.;
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.5, 0.0, 0.0),
+                custom_size: Some(Vec2::new(sprite_size, sprite_size)),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Entity,
+        Kind("Example".to_string()),
+        CommandQueue(vec!(QueueEvent::TravelEvent(Vec2::new(500., 500.)))),
+        // rapier
+        RigidBody::Dynamic,
+        Velocity::zero(),
+        Collider::cuboid(sprite_size / 2., sprite_size / 2.),
+    ));
+}
+
+fn move_entity(rigid_body: &mut Velocity, dest: &Vec2, pos: &Transform) -> bool {
+    let speed = 10.;
+    if pos.translation.x + rigid_body.linvel.x > dest.x { // destination below entity
+        rigid_body.linvel.x = speed;
+    } else if pos.translation.x + rigid_body.linvel.x < dest.x { // destination above entity
+        rigid_body.linvel.x = -speed;
+    } else if pos.translation.x + rigid_body.linvel.x == dest.x {
+        return true;
+    }
+    
+    if pos.translation.y + rigid_body.linvel.y > dest.y { // destination below entity
+        rigid_body.linvel.y = speed;
+    } else if pos.translation.y + rigid_body.linvel.y < dest.y { // destination above entity
+        rigid_body.linvel.y = -speed;
+    } else if pos.translation.y + rigid_body.linvel.y == dest.y {
+        return true;
+    }
+    false
+}
+
+fn event_handler(mut commands: Commands, mut query: Query<(&mut Entity, &mut CommandQueue, &Transform, &mut Velocity)>) {
+    let mut complete = false;
+    for (mut entity, mut queue, pos, mut rigid_body) in &mut query {
+        if !queue.0.is_empty() {
+            println!("handling event");
+            // handle the event in the queue
+            match &queue.0[0] {
+                QueueEvent::TravelEvent(dest) => {
+                    complete = move_entity(&mut rigid_body, dest, pos);
+                },
+                AttackEvent => {
+                    todo!();
+                },
+            }
+            // determine if event has been completed and can be removed from queue
+            if complete {
+                println!("event complete");
+                queue.0.remove(0);
+            }
+        }
+        
+    }
+}
+
+// move player with keyboard
+fn player_controls(
     time: Res<Time>,
     keys: Res<Input<KeyCode>>,
     mut player_query: Query<(&Player, &mut Velocity)>,
@@ -85,8 +156,9 @@ fn main() {
             ..default()
         }))
         .add_startup_system(player_setup)
+        .add_startup_system(entity_setup)
         .add_startup_system(graphics_setup)
-        .add_system(player_movement)
+        .add_system(player_controls)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.))
         .add_plugin(RapierDebugRenderPlugin::default())
         .run();
